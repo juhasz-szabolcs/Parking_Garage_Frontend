95d20543bbdca459be51febe1716bc9c14d62e15
Szabolcs Juhász <juhasz.szabolcs@szbi-pg.hu>
2025-04-11 18:50:41 +0200

    Irányítópult fül eltávolítása (kommentelés)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -100,10 +100,10 @@
                             <span>Statisztika</span>
                         </a>
                     {:else}
-                        <a href="/dashboard" class="nav-link {$page.url.pathname === '/dashboard' ? 'active' : ''}" on:click={toggleMenu}>
+                        <!-- <a href="/dashboard" class="nav-link {$page.url.pathname === '/dashboard' ? 'active' : ''}" on:click={toggleMenu}>
                             <i class="bi bi-speedometer2"></i>
                             <span>Irányítópult</span>
-                        </a>
+                        </a> -->
                         <a href="/cars" class="nav-link {$page.url.pathname === '/cars' ? 'active' : ''}" on:click={toggleMenu}>
                             <i class="bi bi-car-front"></i>
                             <span>Autóim</span>
